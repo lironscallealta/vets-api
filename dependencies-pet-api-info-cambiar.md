9e362605563187340fb4fa6a3fc144c3ef0920b8
lironscallealta <lironscallealta@gmail.com>
2026-05-13 11:38:28 -0400

    feat: initial project setup with Spring Boot 4.0.6 dependencies

---
+++ dependencies-pet-api-info-cambiar.md
@@ -0,0 +1,10 @@
+# API-Mascotas Dependencies
+
+* **Lombok**
+* **Spring Web**
+* **MySQL Driver**
+* **Validation**
+* **Spring Data JPA**
+* **Spring Boot DevTools**
+* **Spring Reactive Web (WebFlux)**
+* **Flyway Migration**
---
+++ src/main/java/cl/duoc/vets_api/VetsApiApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.vets_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetsApiApplication.class, args);
+    }
+}
