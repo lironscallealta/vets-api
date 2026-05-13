2a0f1b3a8682aee66259798fbddbe2add6dc6eef
lironscallealta <lironscallealta@gmail.com>
2026-05-13 16:11:09 -0400

    feat: add Horario entity with relational mapping to Turno

--- src/main/java/cl/duoc/vets_api/model/Horario.java
+++ src/main/java/cl/duoc/vets_api/model/Horario.java
@@ -1,6 +1,7 @@
 package cl.duoc.vets_api.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "horarios")
 public class Horario {
 
     @Id
