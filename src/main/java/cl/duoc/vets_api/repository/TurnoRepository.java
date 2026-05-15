78043825e454f5b5660f5431cc6c57e009ad2f54
lironscallealta <lironscallealta@gmail.com>
2026-05-15 16:29:14 -0400

    feat: implement initial structure for VeterinarioService with stubbed CRUD methods

--- src/main/java/cl/duoc/vets_api/repository/TurnoRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.vets_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cl.duoc.vets_api.model.Turno;
-
-public interface TurnoRepository extends JpaRepository<Turno, Long> {
-
-}
--- src/main/java/cl/duoc/vets_api/service/VeterinarioService.java
+++ src/main/java/cl/duoc/vets_api/service/VeterinarioService.java
@@ -1,19 +1,9 @@
 package cl.duoc.vets_api.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
-
 import cl.duoc.vets_api.dto.request.VeterinarioRequestDto;
-import cl.duoc.vets_api.dto.response.HorarioResponseDto;
-import cl.duoc.vets_api.dto.response.TurnoResponseDto;
 import cl.duoc.vets_api.dto.response.VeterinarioResponseDto;
-import cl.duoc.vets_api.model.Horario;
 import cl.duoc.vets_api.model.Veterinario;
-import cl.duoc.vets_api.repository.HorarioRepository;
-import cl.duoc.vets_api.repository.TurnoRepository;
 import cl.duoc.vets_api.repository.VeterinarioRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -22,111 +12,20 @@ import lombok.RequiredArgsConstructor;
 public class VeterinarioService {
 
     private final VeterinarioRepository veterinarioRepository;
-    private final HorarioRepository horarioRepository;
-    private final TurnoRepository turnoRepository;
 
     private VeterinarioResponseDto mapToVeterinaroToVeterinarioResponse(Veterinario veterinarioModel) {
 
-        VeterinarioResponseDto veterinarioResponse = new VeterinarioResponseDto();
-
-        // 1. Mapeo de datos básicos del Veterinario
-        veterinarioResponse.setId(veterinarioModel.getId());
-        veterinarioResponse.setNumeroRegistroProfesional(veterinarioModel.getNumeroRegistroProfesional());
-        veterinarioResponse.setEgresoProfesional(veterinarioModel.getEgresoProfesional());
-        veterinarioResponse.setEscirujano(veterinarioModel.getEsCirujano());
-
-        // 2. Mapeo de la lista de Horarios
-        List<HorarioResponseDto> listaDeHorariosDto = new ArrayList<>();
-
-        if (veterinarioModel.getHorarios() != null) {
-            for (Horario horario : veterinarioModel.getHorarios()) {
-
-                // Mapeo del Turno lo hice con for la lista y aun no manejo stream bien
-                TurnoResponseDto turnoResponse = new TurnoResponseDto();
-                if (horario.getTurno() != null) {
-                    turnoResponse.setId(horario.getTurno().getId());
-                    turnoResponse.setHorarioTurno(horario.getTurno().getHorarioTurno());
-                    turnoResponse.setHoraInicio(horario.getTurno().getHoraInicio());
-                    turnoResponse.setHoraFin(horario.getTurno().getHoraFin());
-                }
-
-                // Mapeo del Horario
-                HorarioResponseDto horarioResponse = new HorarioResponseDto();
-                horarioResponse.setId(horario.getId());
-                horarioResponse.setDia(horario.getDia());
-
-                // asocio turno a horario
-                horarioResponse.setTurno(turnoResponse);
-
-                // agrego lista final
-                listaDeHorariosDto.add(horarioResponse);
-            }
-        }
-
-        veterinarioResponse.setHorario(listaDeHorariosDto);
-
-        return veterinarioResponse;
+        return null;
     }
 
     public VeterinarioResponseDto registrarVeterinario(VeterinarioRequestDto veterinarioRequest) {
 
-        Veterinario veterinarioModel = new Veterinario();
-
-        List<Horario> horarios = horarioRepository.findAllById(veterinarioRequest.getIdHorarios());
-
-        if (horarios.isEmpty()) {
-
-            throw new RuntimeException("Error al crear veterinario, no se encontraron horarios");
-
-        }
-
-        veterinarioModel.setEsCirujano(veterinarioRequest.getEscirujano());
-        veterinarioModel.setEgresoProfesional(veterinarioRequest.getEgresoProfesional());
-        veterinarioModel.setNumeroRegistroProfesional(veterinarioRequest.getNumeroRegistroProfesional());
-        veterinarioModel.setHorarios(horarios);
-        veterinarioModel.setEsCirujano(veterinarioRequest.getEscirujano());
-
-        VeterinarioResponseDto response = mapToVeterinaroToVeterinarioResponse(veterinarioModel);
-
-        return response;
+        return null;
 
     }
 
     public VeterinarioResponseDto ConsultarVeterinarioId(Long veterinarioidId) {
-
-        Optional<Veterinario> veterinario = veterinarioRepository.findById(veterinarioidId);
-
-        if (veterinario.isEmpty()) {
-
-            throw new RuntimeException("Error al buscar veterinario, no se encontro");
-
-        }
-
-        VeterinarioResponseDto response = mapToVeterinaroToVeterinarioResponse(veterinario.get());
-
-        return response;
-
-    }
-
-    public List<VeterinarioResponseDto> ConsultarVeterinarios() {
-
-        List<Veterinario> veterinarios = veterinarioRepository.findAll();
-        List<VeterinarioResponseDto> responseList = new ArrayList<>();
-
-        if (veterinarios.isEmpty()) {
-
-            throw new RuntimeException("No hay veterinarios registrados");
-
-        }
-
-        for (Veterinario veterinario : veterinarios) {
-
-            VeterinarioResponseDto response = mapToVeterinaroToVeterinarioResponse(veterinario);
-            responseList.add(response);
-
-        }
-
-        return responseList;
+        return null;
 
     }
 
