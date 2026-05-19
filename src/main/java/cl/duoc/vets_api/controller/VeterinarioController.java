7791ce6672fcc5830db682fee0c77559c7bae5da
lironscallealta <lironscallealta@gmail.com>
2026-05-19 17:30:40 -0400

    feat: implement VeterinarioController with CRUD operations for veterinarian management

--- src/main/java/cl/duoc/vets_api/controller/VeterinarioController.java
+++ src/main/java/cl/duoc/vets_api/controller/VeterinarioController.java
@@ -9,8 +9,15 @@ package cl.duoc.vets_api.controller;
 import cl.duoc.vets_api.dto.request.VeterinarioRequestDto;
 import cl.duoc.vets_api.dto.response.VeterinarioResponseDto;
 import cl.duoc.vets_api.service.VeterinarioService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,29 +28,34 @@ public class VeterinarioController {
 
     private final VeterinarioService veterinarioService;
 
-    public ResponseEntity<VeterinarioResponseDto> registrarVeterinario(VeterinarioRequestDto veterinarioRequest) {
+    @PostMapping
+    public ResponseEntity<VeterinarioResponseDto> registrarVeterinario(
+            @Valid @RequestBody VeterinarioRequestDto veterinarioRequest) {
 
         VeterinarioResponseDto response = veterinarioService.registrarVeterinario(veterinarioRequest);
 
         return ResponseEntity.ok(response);
     }
 
-    public ResponseEntity<VeterinarioResponseDto> ConsultarVeterinarioId(Long id) {
+    @GetMapping
+    public ResponseEntity<VeterinarioResponseDto> ConsultarVeterinarioId(@PathVariable Long id) {
 
         VeterinarioResponseDto response = veterinarioService.ConsultarVeterinarioId(id);
 
         return ResponseEntity.ok(response);
     }
 
+    @PutMapping("/{id}")
     public ResponseEntity<VeterinarioResponseDto> actualizarVeterinario(
-            Long id, VeterinarioRequestDto veterinarioRequest) {
+            @PathVariable Long id, @Valid @RequestBody VeterinarioRequestDto veterinarioRequest) {
 
         VeterinarioResponseDto response = veterinarioService.actualizarVeterinario(id, veterinarioRequest);
 
         return ResponseEntity.ok(response);
     }
 
-    public ResponseEntity<VeterinarioResponseDto> eliminarVeterinario(Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<VeterinarioResponseDto> eliminarVeterinario(@PathVariable Long id) {
         VeterinarioResponseDto response = veterinarioService.eliminarVeterinario(id);
         return ResponseEntity.ok(response);
     }
