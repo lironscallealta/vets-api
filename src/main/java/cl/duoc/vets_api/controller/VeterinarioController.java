9cd399b8c9ea8f05e69a5fe59f2dace4f21bca20
lironscallealta <lironscallealta@gmail.com>
2026-05-19 17:41:48 -0400

    feat: implement VeterinarioController with CRUD operations for managing veterinarians

--- src/main/java/cl/duoc/vets_api/controller/VeterinarioController.java
+++ src/main/java/cl/duoc/vets_api/controller/VeterinarioController.java
@@ -9,15 +9,16 @@ package cl.duoc.vets_api.controller;
 import cl.duoc.vets_api.dto.request.VeterinarioRequestDto;
 import cl.duoc.vets_api.dto.response.VeterinarioResponseDto;
 import cl.duoc.vets_api.service.VeterinarioService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,10 +35,19 @@ public class VeterinarioController {
 
         VeterinarioResponseDto response = veterinarioService.registrarVeterinario(veterinarioRequest);
 
-        return ResponseEntity.ok(response);
-    }
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    } /*
+       *
+       * try {
+       * Fiesta registar = fiestaService.registrarFiesta(nuevaFiesta);
+       * return ResponseEntity.status(HttpStatus.CREATED).body(registar);
+       * } catch (IllegalArgumentException e) {
+       * return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("Error",
+       * "Codigo Existe"));
+       * }
+       */
 
-    @GetMapping
+    @GetMapping("/{id}")
     public ResponseEntity<VeterinarioResponseDto> ConsultarVeterinarioId(@PathVariable Long id) {
 
         VeterinarioResponseDto response = veterinarioService.ConsultarVeterinarioId(id);
