415ad185449afddf87f1afd25ca4aded735a3cb0
polirritmico <ebray187@gmail.com>
2026-06-11 00:03:40 -0400

    feat: add VetScheduleResponse response DTO

---
+++ src/main/java/cl/duoc/vets_api/dto/response/VetScheduleResponse.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.vets_api.dto.response;
+
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VetScheduleResponse {
+    private Long vetId;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+}
