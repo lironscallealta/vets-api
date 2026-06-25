aa71887a6399a7afd6b1cfec4261dfed4531dfbd
lironscallealta <lironscallealta@gmail.com>
2026-06-25 19:42:25 -0400

    chore: configure Eureka client following springboot-layout

--- src/main/java/cl/duoc/vets_api/VetsApiApplication.java
+++ src/main/java/cl/duoc/vets_api/VetsApiApplication.java
@@ -9,8 +9,10 @@ package cl.duoc.vets_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class VetsApiApplication {
 
     public static void main(String[] args) {
